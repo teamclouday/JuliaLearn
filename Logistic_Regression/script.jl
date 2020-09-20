84a7983b4965be6b423127c49998a0c2c2f417be
teamclouday <teamclouday@gmail.com>
2020-09-20 00:50:47 -0400

    add logistic regression

---
+++ Logistic_Regression/script.jl
@@ -0,0 +1,188 @@
+# Logistic Regression Module
+module LogReg
+export train, predict, predict_proba, scale, cost, accuracy
+
+import Random
+import Statistics
+
+"""
+Helper function to scale input `X` to unit variance\\
+`X` is required to have dimension 2
+"""
+function scale(X::Array)::Array
+    @assert ndims(X) == 2
+    u = Statistics.mean(X, dims=1)
+    s = Statistics.std(X, dims=1)
+    res = (X .- u) ./ s
+    return res
+end
+
+"""
+Sigmoid function on array `Z`
+"""
+function sigmoid(Z::Array)::Array
+    denom = 1 .+ (MathConstants.e .^ (-Z))
+    return 1 ./ denom
+end
+
+"""
+Cost function for logistic Regression\\
+If `X` is shape (M, N)\\
+`y` should be (M,)\\
+`beta` should be (N+1,)
+------
+May not be used
+"""
+function cost(X::Array, y::Array, beta::Array)::AbstractFloat
+    @assert ndims(X) == 2
+    @assert ndims(y) == 1
+    @assert ndims(beta) == 1
+    @assert size(X) == (size(y)[1], size(beta)[1]-1)
+    m = size(X)[1]
+    X_extended = hcat(X, ones(size(X)[1]))
+    X_combined = X_extended * reshape(beta, (size(beta)[1], 1))
+    prob = sigmoid(X_combined)
+    y_prep = reshape(y, (size(y)[1], 1))
+    vec = y_prep .* (log.(prob)) .+ (1 .- y_prep) .* (log.(1 .- prob))
+    cost = -1 / float(m) * sum(vec)
+    return cost
+end
+
+"""
+Predict function for Logistic Regression\\
+If `X` is shape (M, N)\\
+`beta` should be (N+1,)\\
+Returns 1d array of real probabilities
+"""
+function predict_proba(X::Array, beta::Array)::Array
+    @assert ndims(X) == 2
+    @assert ndims(beta) == 1
+    @assert size(X)[2] == size(beta)[1]-1
+    X_extended = hcat(X, ones(size(X)[1]))
+    X_combined = X_extended * reshape(beta, (size(beta)[1], 1))
+    prob = sigmoid(X_combined)
+    prob = [(prob...)...] # flatten to 1d array
+    return prob
+end
+
+"""
+Predict function for Logistic Regression\\
+If `X` is shape (M, N)\\
+`beta` should be (N+1,)\\
+Returns 1d array of 0,1
+"""
+function predict(X::Array, beta::Array)::Array
+    @assert ndims(X) == 2
+    @assert ndims(beta) == 1
+    @assert size(X)[2] == size(beta)[1]-1
+    X_extended = hcat(X, ones(size(X)[1]))
+    X_combined = X_extended * reshape(beta, (size(beta)[1], 1))
+    prob = sigmoid(X_combined)
+    prob = [(prob...)...] # flatten to 1d array
+    real_prob::Array{Integer} = map(m -> m >= 0.5 ? 1 : 0, prob)
+    return real_prob
+end
+
+"""
+Learning function for Gradient Descent\\
+If `X` is shape (M, N)\\
+`y` should be (M,)\\
+`beta` should be (N+1,)
+------
+Returns `nothing`
+------
+Note: `beta` will be updated inplace
+"""
+function learn!(X::Array, y::Array, beta::Array, alpha::AbstractFloat)
+    @assert ndims(X) == 2
+    @assert ndims(y) == 1
+    @assert ndims(beta) == 1
+    @assert size(X) == (size(y)[1], size(beta)[1]-1)
+    predictions = predict_proba(X, beta)
+    offset = predictions .- y
+    offset = reshape(offset, (size(offset)[1], 1))
+    X_extended = hcat(X, ones(size(X)[1]))
+    gradients = X_extended' * offset
+    gradients = gradients ./ size(X)[1]
+    gradients = gradients .* alpha
+    beta .= beta .- [(gradients...)...]
+    return nothing
+end
+
+"""
+Learning function for Gradient Descent\\
+If `X` is shape (M, N)\\
+`y` should be (M,)\\
+`beta` should be (N+1,)
+------
+Returns updated `beta`
+"""
+function learn(X::Array, y::Array, beta::Array, alpha::AbstractFloat)::Array
+    @assert ndims(X) == 2
+    @assert ndims(y) == 1
+    @assert ndims(beta) == 1
+    @assert size(X) == (size(y)[1], size(beta)[1]-1)
+    predictions = predict_proba(X, beta)
+    offset = predictions .- y
+    offset = reshape(offset, (size(offset)[1], 1))
+    X_extended = hcat(X, ones(size(X)[1]))
+    gradients = X_extended' * offset
+    gradients = gradients ./ size(X)[1]
+    gradients = gradients .* alpha
+    beta = beta .- [(gradients...)...]
+    return beta
+end
+
+"""
+Training function for Logisitic Regression\\
+Implemented using Gradient Descent\\
+If `X` is shape (M, N)\\
+`y` should be (M,)\\
+`max_iter` should be >= 0
+------
+if `return_all`, then return all `beta`(weights) for each iteration\\
+else, return the final `beta`(weight) after iterations
+"""
+function train(X::Array, y::Array; learning_rate::AbstractFloat=0.01, max_iter::Integer=100, return_all::Bool=false)::Array
+    @assert ndims(X) == 2
+    @assert ndims(y) == 1
+    @assert size(X)[1] == size(y)[1]
+    @assert max_iter >= 0
+    beta = Random.randn(size(X)[2]+1)
+    res = nothing
+    if return_all
+        res = reshape(beta, (1, size(beta)[1]))
+    else
+        res = beta
+    end
+    for i = 1:max_iter
+        if return_all
+            beta = learn(X, y, beta, learning_rate)
+            res = cat(res, reshape(beta, (1, size(beta)[1])), dims=1)
+        else
+            learn!(X, y, beta, learning_rate)
+            res .= beta
+        end
+    end
+    return res
+end
+
+"""
+Accuracy function for Logisitic Regression\\
+`y_pred` and `y_real` should have same shape, 1d array
+------
+Returns accuracy as float
+"""
+function accuracy(y_pred::Array, y_real::Array)::AbstractFloat
+    @assert ndims(y_pred) == ndims(y_real) == 1
+    @assert size(y_pred) == size(y_real)
+    sum = 0
+    for (m, n) in zip(y_pred, y_real)
+        if m == n
+            sum += 1
+        end
+    end
+    return sum / size(y_pred)[1]
+end
+
+end
